86c7e0e8a4529e4b1e14d5e5c0405c9817e119d3
NathanPB <nathan.pbombana@gmail.com>
2019-10-18 05:00:38 -0300

    Moved the flights menu options to the main menu

--- src/main/java/br/upf/ads/aeroporto/cli/menus/CLIInitialMenuRunnable.java
+++ src/main/java/br/upf/ads/aeroporto/cli/menus/CLIInitialMenuRunnable.java
@@ -3,7 +3,8 @@ package br.upf.ads.aeroporto.cli.menus;
 import br.upf.ads.aeroporto.cli.CLI;
 import br.upf.ads.aeroporto.cli.CLIMenuRunnable;
 import br.upf.ads.aeroporto.cli.CLIMenuCollection;
-import br.upf.ads.aeroporto.cli.menus.flyghts.CLIFlyghtsMenuRunnable;
+import br.upf.ads.aeroporto.cli.menus.flyghts.CLIFlyghtsCurrentMenuRunnable;
+import br.upf.ads.aeroporto.cli.menus.flyghts.CLIFlyghtsReportMenuRunnable;
 import br.upf.ads.aeroporto.utils.DateTimeUtils;
 
 public class CLIInitialMenuRunnable extends CLIMenuRunnable {
@@ -12,8 +13,9 @@ public class CLIInitialMenuRunnable extends CLIMenuRunnable {
 
     public CLIInitialMenuRunnable(CLI cli) {
         super(cli);
-         menuList.add(new CLIFlyghtsMenuRunnable(cli, this));
-         menuList.add(new CLIExitMenuRunnable(cli));
+        menuList.add(new CLIFlyghtsReportMenuRunnable(cli));
+        menuList.add(new CLIFlyghtsCurrentMenuRunnable(cli));
+        menuList.add(new CLIExitMenuRunnable(cli));
     }
 
     @Override
