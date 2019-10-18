4e8de07831efe41ab42e7baedf0ba2b5ff86206b
NathanPB <nathan.pbombana@gmail.com>
2019-10-18 04:57:55 -0300

    Menu runs in loop

--- src/main/java/br/upf/ads/aeroporto/Main.java
+++ src/main/java/br/upf/ads/aeroporto/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static CLIMenuRunnable initialMenu = new CLIInitialMenuRunnable(cli);
 
     public static void main(String[] args) {
-        initialMenu.run();
+        while(true) initialMenu.run();
     }
 }
